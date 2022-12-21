d5cb99bc806ec5636952ba82198b099b7b04a1d6
«Valeev_Andrey» <Valeevptk@yandex.ru>
2022-12-21 18:27:39 +0300

    ADD lec1.7

---
+++ lec1.7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int xa = 1, ya = 1;
+int xb = 1, yb = 30;
+int xc = 40, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
